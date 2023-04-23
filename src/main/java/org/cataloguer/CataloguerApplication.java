77e4a99d492e8561738eae47c447160d9063ca3c
msvdev-k <msvdev.k@gmail.com>
2023-04-23 14:25:46 +0300

    init commit

---
+++ src/main/java/org/cataloguer/CataloguerApplication.java
@@ -0,0 +1,13 @@
+package org.cataloguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CataloguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CataloguerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cataloguer/CataloguerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cataloguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CataloguerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
