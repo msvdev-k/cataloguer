dd9dd90422168b2566c5632387b6279a880f2e3c
msvdev-k <msvdev.k@gmail.com>
2023-05-11 16:17:02 +0300

    Добавлены репозитории для сущностей User, Catalog, Card и FieldTemplate

---
+++ src/main/java/org/cataloguer/repository/CatalogRepository.java
@@ -0,0 +1,9 @@
+package org.cataloguer.repository;
+
+import org.cataloguer.entity.CatalogEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+}
